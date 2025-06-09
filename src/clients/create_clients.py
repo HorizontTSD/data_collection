8c69898977f00ad47649183e91e4735e44f73e90
Nikita <savvin.nikit@yandex.ru>
2025-06-09 08:40:35 +0300

    feat: Оформил репозиторий

---
+++ src/clients/create_clients.py
@@ -0,0 +1,12 @@
+import os
+import psycopg2
+
+
+def get_db_connection():
+    return psycopg2.connect(
+        dbname=os.getenv("PG_DB"),
+        user=os.getenv("PG_USER"),
+        password=os.getenv("PG_PASSWORD"),
+        host=os.getenv("PG_HOST"),
+        port=os.getenv("PG_PORT")
+    )
