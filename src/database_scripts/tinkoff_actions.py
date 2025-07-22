e1afbed2b36533162b507b95d1bb801e1e8e948e
Tatyana <tgutsulyak@mail.ru>
2025-07-22 21:07:51 +0300

    добавлена часть ф-й обработки бд

--- src/database_scripts/tinkoff_actions.py
+++ src/database_scripts/tinkoff_actions.py
@@ -1,30 +1,57 @@
 from fastapi import FastAPI
-from datetime import datetime
+from datetime import datetime, timezone
 from datetime import timedelta
 from pandas import DataFrame
+import pandas as pd
+from tinkoff.invest import Client
+import json
 
 
-TINKOFF_TOKEN = '???'
-DB_URL = '???'
+TINKOFF_TOKEN = 't.kF5HrpicTNIofE9aAnCDKRBFTXNwyfm9ypsVhCFQxCE3cVOucjUMhpYYxM1V5teev9eaI1_9aYnFWnM71uy97w'
 
 def get_last_datetime(db):
-    return
+    last_datetime = datetime(1000,1,1)
+    for share in db:
+        if share.first_1day_candle_date > last_datetime.replace(tzinfo=timezone.utc) :
+            last_datetime = share.first_1day_candle_date
 
-def get_new_data(last_db_datetime: datetime, current_datetime: datetime):
-    return
+    return last_datetime
 
-def handle_data_filtration(db: DataFrame):
-    return
+def get_new_data(last_db_datetime: datetime, current_datetime: datetime, db):
+    new_data = []
+    if current_datetime >= last_db_datetime:
+        for share in db:
+            if share.first_1day_candle_date == last_db_datetime:
+                new_data.append(share)
+    else:
+        pass
+
+    return new_data
+
+def handle_data_filtration(df: DataFrame):
+    df = df.drop_duplicates(subset=["figi"])
+    # ?
+
+    return df
 
 def add_in_database(db: DataFrame):
     pass
 
 
-current_date = datetime.today()
-current_date = current_date.strftime('%d.%m.%y')
+current_date = datetime.today().replace(tzinfo=timezone.utc)
 
 endpoint = FastAPI()
 
+with Client(TINKOFF_TOKEN) as client:
+    shares_db = client.instruments.shares()
+
+last_datetime_share = get_last_datetime(shares_db.instruments)
+fresh_data = get_new_data(last_datetime_share, current_date, shares_db.instruments)
+fresh_data = pd.DataFrame(fresh_data)
+
+df_unique = handle_data_filtration(fresh_data)
+
+a=1
 
 
 
