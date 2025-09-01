c5b59b9775cbc9c4f356eb9a6f8fa898dbde97b0
Tatyana <tgutsulyak@mail.ru>
2025-09-01 21:15:38 +0300

    переделана ф-я поиска свеч

--- src/database_scripts/tinkoff_actions.py
+++ src/database_scripts/tinkoff_actions.py
@@ -7,6 +7,7 @@ import pandas as pd
 from tinkoff.invest import Client
 from dotenv import load_dotenv
 import os
+import json
 
 load_dotenv()
 TINKOFF_TOKEN = os.getenv('TINKOFF_TOKEN')
@@ -42,72 +43,21 @@ def fetch_data_from_db() -> DataFrame:
     return df_result
 
 
+def get_candle_date_from_db(data:list) -> dict:
+    dict_figi_time = []
 
-def get_last_datetime(data:list) -> datetime:
-    # last_datetime = datetime(1000,1,1)
-    dict_figi_time = {}
-
-    # TODO на выходе словарь ключ-акция: время записи1candle в формате json
     for share in data:
-        dict_figi_time[share.figi] = share.first_1min_candle_date
+        dict_figi_time.append(share.first_1min_candle_date.replace(tzinfo=None))
 
+    return dict_figi_time
 
-# при чем тут нижнее? json - просто словарь?
-
-        #if share.first_1min_candle_date > last_datetime.replace(tzinfo=timezone.utc) :
-        #    last_datetime = share.first_1min_candle_date
-        #else:
-          #  pass
-
-    return last_datetime
-
-def get_new_data(last_db_datetime: datetime, current_datetime: datetime, db):
-    #TODO все к черту
-    # тинькоф- бд поискать в документации.
-    # dbparams - наше. не коммитить логины. из
-    # как вытащить дагнные по конкретной акции.
-    new_data = []
-    if current_datetime >= last_db_datetime:
-        for share in db:
-            if share.first_1day_candle_date == last_db_datetime:
-                new_data.append(share)
-    else:
-        pass
-
-    return new_data
-
-
-def handle_data_filtration(df: DataFrame):
-    df = df.drop_duplicates(subset=["figi"])
-    # ?
-    #df.to_csv('data.csv', encoding='utf-8')
-    return df
-
-def add_in_database(db: DataFrame):
-    pass
-
-
-current_date = datetime.today().replace(tzinfo=timezone.utc)
-endpoint = FastAPI()
 
 with Client(TINKOFF_TOKEN) as client:
     shares_connection = client.instruments.shares()
 
 
+candle_date = get_candle_date_from_db(shares_connection.instruments)
 
-
-#df = fetch_data_from_db()
-#df_init = df[:-288]
-#df_test = df[-288:]
-
-
-last_datetime_share = get_last_datetime(shares_connection.instruments)
-'''
-fresh_data = get_new_data(last_datetime_share, current_date, shares_connection.instruments)
-fresh_data = pd.DataFrame(fresh_data)
-
-df_unique = handle_data_filtration(fresh_data)
-'''
 a=1
 
 
